04bc11d94044974e220c5f971ed83366d857bdfd
jpribeir <joaop.ribeiro@outlook.pt>
2022-12-11 20:04:30 +0000

    Day 7 part 2 complete with comments

--- src/day7.py
+++ src/day7.py
@@ -1,7 +1,6 @@
 # Day 7 of the 2022 Advent of Code
 # Class to instantiate directories, detailing its parent directory and child directories/files
 class Dir():
-    # Initiate the object
     def __init__(self,name,parent):
         self.name = name
         self.parent = parent
@@ -16,19 +15,21 @@ class Dir():
         self.files_dict[file] = size
         self.total_size += int(size)
 
+# Go through directory tree recursively updating variable when finding a new lower eligible value
+def goInto(dir,delfolder_size):
+    if dir.total_size < delfolder_size and dir.total_size >= minsize_to_delete: delfolder_size = dir.total_size
+    for child in dir.children_dict.values(): delfolder_size = goInto(child,delfolder_size)
+    return delfolder_size
+
 # Read input file
-#with open("../include/input7.inc","r") as cmd_file:
-with open("../include/example7.inc","r") as cmd_file:
+with open("../include/input7.inc","r") as cmd_file:
     cmd_list = list(map(lambda a: a.strip(),cmd_file.readlines()))
 
 root = Dir("/","/")
 current_node = root
 sizecap = 100000
 total_sum = 0
-minsize_to_delete = 30000000
-delfolder_size = 70000000
 for cmd in cmd_list[1:]:
-    #print("### %s"%cmd)
     if cmd.startswith("$ cd"):
         if cmd.split()[-1] == "..":
             if current_node.total_size<=sizecap: total_sum += current_node.total_size
@@ -38,16 +39,18 @@ for cmd in cmd_list[1:]:
     elif not cmd.startswith("$"):
         if cmd.startswith("dir"): current_node.addChild(cmd.split()[1])
         else: current_node.addFile(cmd.split()[1],cmd.split()[0])
-    #print("%s -> %s"%(current_node.name,current_node.total_size))
 
-# Finish up the size count for outermost directories
+# Finish up the size count for outermost directories (since the input doesn't "cd.." the last directories)
 while current_node.name!="/":
     if current_node.total_size <=sizecap: total_sum += current_node.total_size
-    if current_node.total_size<delfolder_size and current_node.total_size>=minsize_to_delete: delfolder_size = current_node.total_size
     (current_node.parent).total_size += current_node.total_size
     current_node = current_node.parent
-minsize_to_delete = 30000000 - (70000000 - root.total_size)
-#NOW PART2: search tree for smallest folder that's bigger than minsize_to_delete
+
+# Recursively find the lowest size directory bigger than minsize_to_delete in the tree
+total_space = 70000000
+needed_free_space = 30000000
+minsize_to_delete = needed_free_space - (total_space - root.total_size)
+delfolder_size = goInto(root,needed_free_space)
 
 print("Part1: %s"%total_sum)
-#print("Part2: %s"%delfolder_size)
+print("Part2: %s"%delfolder_size)

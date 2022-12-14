389a6f394c7c3b5fdc12bced5226c426084089ad
Joao Pedro Ribeiro <jpribeir@synopsys.com>
2022-12-14 16:29:12 +0000

    Day 2 part complete with comments

--- src/day5.py
+++ src/day5.py
@@ -1,13 +1,22 @@
 # Day 5 of the 2022 Advent of Code
-import re
+# Depending on part the crates are moved in the same order or reversed between stacks
+def moveCrates(cargo_stack,move_list,multiple_crates_en):
+    for move in move_list:
+        num_crates = int((move.split("move ")[1]).split(" from")[0])
+        orig_index = int(move.split("from ")[1][0])-1
+        dest_index = int(move.split("to ")[1][0])-1
+        moving_crates = cargo_stack[orig_index][0:num_crates]
+        if multiple_crates_en: cargo_stack[dest_index] = moving_crates+cargo_stack[dest_index]
+        else: cargo_stack[dest_index] = moving_crates[::-1]+cargo_stack[dest_index]
+        cargo_stack[orig_index] = cargo_stack[orig_index][num_crates:]
+    return "".join(list(x[0] for x in cargo_stack))
 
 # Read input file
-#with open("../include/example5.inc","r") as cargo_file:
 with open("../include/input5.inc","r") as cargo_file:
     cargo_list = cargo_file.readlines()
 
-num_stacks = int(len(cargo_list[0])/4)
-cargo_stack = [ "" ] * num_stacks
+# Prepare list of stacks, number of characters in a line is mutiple of the number of stacks
+cargo_stack = [ "" ] * int(len(cargo_list[0])/4)
 move_list = []
 for line in cargo_list:
     if line.startswith("move"): move_list.append(line.strip())
@@ -17,12 +26,5 @@ for line in cargo_list:
             if line[i] != " ": cargo_stack[j]+=line[i]
             j += 1
 
-for move in move_list:
-    num_crates = int(move.split("move ")[1][0])
-    orig_stack = int(move.split("from ")[1][0])-1
-    dest_stack = int(move.split("to ")[1][0])-1
-    moving_crates = cargo_stack[orig_stack][0:num_crates]
-    cargo_stack[orig_stack] = cargo_stack[orig_stack][num_crates:]
-    cargo_stack[dest_stack] = moving_crates[::-1]+cargo_stack[dest_stack]
-
-print(cargo_stack)
+print("Part1: %s"%moveCrates(cargo_stack.copy(),move_list,False))
+print("Part2: %s"%moveCrates(cargo_stack.copy(),move_list,True))

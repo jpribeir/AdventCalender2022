5ed1d5fedb5fd3f3a20b01f8782b7c315dcf358f
jpribeir <joaop.ribeiro@outlook.pt>
2022-12-15 00:49:37 +0000

    Day 8 part 2 complete with comments

--- src/day8.py
+++ src/day8.py
@@ -9,34 +9,35 @@ def moveInDirection(i,j,incr_i,incr_j,min_height,view_count):
     if foundEdge(i,j): return True,view_count
     elif tree_matrix[i+incr_i][j+incr_j]<min_height:
         view_count += 1
-        return moveInDirection(i+incr_i,j+incr_j,incr_i,incr_j,min_height)
-    else:
-        view_count += 1
-        return False,view_count
+        return moveInDirection(i+incr_i,j+incr_j,incr_i,incr_j,min_height,view_count)
+    else: return False,view_count+1
 
 # Check line of sight in all 4 basic directions
 def checkVisability(i,j):
+    is_visible = False
+    view_factor = 1
     for incr_i,incr_j in [(-1,0),(0,1),(1,0),(0,-1)]:
-        view_count = 1
         if tree_matrix[i][j]>tree_matrix[i+incr_i][j+incr_j]:
-            if moveInDirection(i+incr_i,j+incr_j,incr_i,incr_j,tree_matrix[i][j],view_count): return True
-    else: return False
+            found_view,view_count = moveInDirection(i+incr_i,j+incr_j,incr_i,incr_j,tree_matrix[i][j],1)
+            if found_view: is_visible = True
+            view_factor *= view_count
+    return is_visible,view_factor
 
 # Read input file
-with open("../include/example8.inc","r") as tree_file:
-#with open("../include/input8.inc","r") as tree_file:
+with open("../include/input8.inc","r") as tree_file:
     tree_matrix = list(map(lambda a: a.strip(),tree_file.readlines()))
 
-max_i = len(tree_matrix)-1
-max_j = len(tree_matrix[0])-1
-#hidden_matrix = [ [ False ] * max_j ] * max_i
-visible_count = 0
+max_i , max_j = len(tree_matrix)-1 , len(tree_matrix[0])-1
+visible_count , max_view = 0 , 1 
+
+# Go through each tree in each line
 for i,line in enumerate(tree_matrix):
     for j,tree in enumerate(line):
-        #print("### %s,%s ###"%(i,j))
         if foundEdge(i,j): visible_count += 1
-        elif checkVisability(i,j): visible_count += 1
-        #else: hidden_matrix[i][j] = True
+        else:
+            is_visible,view_factor = checkVisability(i,j)
+            if is_visible: visible_count += 1
+            max_view = max(max_view,view_factor)
 
 print("Part1: %s"%visible_count)
-#print("Part2: %s"%visible_count)
+print("Part2: %s"%max_view)

9a0a673fbc6342033990679ee32e3cffe4129c1c
Joao Pedro Ribeiro <jpribeir@synopsys.com>
2022-12-30 10:11:41 +0000

    Day 10 code simplified, part 2 output clearer, and with comments

--- src/day10.py
+++ src/day10.py
@@ -2,31 +2,6 @@
 def validList(num):
     return list(range(num,num+3))
 
-def part1():
-    X = 1
-    cycle_count = 0
-    for line in cycle_list:
-        cycle_count += 1
-        if cycle_count in interesting_dict.keys(): interesting_dict[cycle_count] = X
-        if line.split(" ")[0] == "addx":
-            cycle_count += 1
-            if cycle_count in interesting_dict.keys(): interesting_dict[cycle_count] = X
-            X += int(line.split(" ")[1])
-    return sum(map(lambda a,b: a*b,interesting_dict.keys(),interesting_dict.values()))
-
-def part2():
-    screen_matrix = ["."*40]*6
-    valid_list = validList(0)
-    cycle_count = 0
-    for line in cycle_list:
-        if int(cycle_count%40) in valid_list: screen_matrix[int(cycle_count/40)] = screen_matrix[int(cycle_count/40)][0:(cycle_count%40)]+"#"+screen_matrix[int(cycle_count/40)][(cycle_count%40)+1:]
-        cycle_count += 1
-        if line.split(" ")[0] == "addx":
-            if int(cycle_count%40) in valid_list: screen_matrix[int(cycle_count/40)] = screen_matrix[int(cycle_count/40)][0:(cycle_count%40)]+"#"+screen_matrix[int(cycle_count/40)][(cycle_count%40)+1:]
-            valid_list = validList(valid_list[0]+int(line.split(" ")[1]))
-            cycle_count += 1
-    for line in screen_matrix: print(line)
-
 # Read input file
 with open("../include/input10.inc","r") as cycle_file:
     cycle_list = list(map(lambda a: a.strip(),cycle_file.readlines()))
@@ -37,7 +12,26 @@ interesting_dict = {20:0,
                     140:0,
                     180:0,
                     220:0}
+X = 1
+screen_matrix = ["."*40]*6
+valid_list = validList(0)
+cycle_count = 0
+for line in cycle_list:
+    # Print "@" if pixel is one of 3 valid indexes
+    if int(cycle_count%40) in valid_list: screen_matrix[int(cycle_count/40)] = screen_matrix[int(cycle_count/40)][0:(cycle_count%40)]+"@"+screen_matrix[int(cycle_count/40)][(cycle_count%40)+1:]
+    cycle_count += 1
 
-print("Part1: %s"%part1())
-print("\nPart2:")
-part2()
+    # Store X if cycle is interesting
+    if cycle_count in interesting_dict.keys(): interesting_dict[cycle_count] = X
+
+    # If addx do all again in new cycle, and update X and valid indexes
+    if line.split(" ")[0] == "addx":
+        if int(cycle_count%40) in valid_list: screen_matrix[int(cycle_count/40)] = screen_matrix[int(cycle_count/40)][0:(cycle_count%40)]+"@"+screen_matrix[int(cycle_count/40)][(cycle_count%40)+1:]
+        valid_list = validList(valid_list[0]+int(line.split(" ")[1]))
+        cycle_count += 1
+        if cycle_count in interesting_dict.keys(): interesting_dict[cycle_count] = X
+        X += int(line.split(" ")[1])
+print("Part1: %s"%sum(map(lambda a,b: a*b,interesting_dict.keys(),interesting_dict.values())))
+print("Part2:")
+for line in screen_matrix:
+    print(" ".join(line)) #this method puts a space between each character

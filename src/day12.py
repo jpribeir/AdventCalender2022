ac78b9b839dd8f26dd227c6b384b7cc0cf10eeae
Joao Pedro Ribeiro <jpribeir@synopsys.com>
2023-01-03 16:57:11 +0000

    Day 12 parts 1 and 2 complete with comments

--- src/day12.py
+++ src/day12.py
@@ -1,49 +1,72 @@
 # Day 12 of the 2022 Advent of Code
+# Return x,y point where character is found
 def findChar(char):
     for i,line in enumerate(heightmap_matrix):
         if char in line:
             for j,letter in enumerate(line):
                 if letter==char: return i,j
 
+# Return unicode code for character
 def heightVal(char):
     if char=="S": return ord("a")
     elif char=="E": return ord("z")
     else: return ord(char)
 
-def changeMap(visited_matrix,x,y,char):
-    newly_visited_matrix = visited_matrix.copy()
-    newly_visited_matrix[x] = newly_visited_matrix[x][0:y]+char+newly_visited_matrix[x][y+1:]
-    return newly_visited_matrix
-
-def nextSpot(x,y,lenval,visited_matrix,prevX,prevY,lowest_path):
-    if lenval + sum(map(lambda a,b: abs(a-b), [endX,endY],[x,y])) < lowest_path:
-        #print("## %s,%s ##"%(x,y))
-        newly_visited_matrix = changeMap(visited_matrix,x,y,"#")
-        #if x==prevX and y==prevY: newly_visited_matrix = changeMap(visited_matrix,x,y,"E")
-        #elif x-prevX==1: newly_visited_matrix = changeMap(visited_matrix,x,y,"^")
-        #elif x-prevX==-1: newly_visited_matrix = changeMap(visited_matrix,x,y,"v")
-        #elif y-prevY==1: newly_visited_matrix = changeMap(visited_matrix,x,y,"<")
-        #elif y-prevY==-1: newly_visited_matrix = changeMap(visited_matrix,x,y,">")
-        #print(newly_visited_matrix)
-        if heightmap_matrix[x][y]=="S":
-            #print("## %s ##"%lenval)
-            #for each in newly_visited_matrix: print(each)
-            return lenval
-        for i,j in [(-1,0),(1,0),(0,-1),(0,1)]:
-            #print(">%s,%s"%(x+i,x+j))
-            if (x+i) in range(0,maxX) and (y+j) in range(0,maxY) and newly_visited_matrix[x+i][y+j]==".":
-                if heightVal(heightmap_matrix[x+i][y+j])-heightVal(heightmap_matrix[x][y])>=-1:
-                    lowest_path = min(lowest_path,nextSpot(x+i,y+j,lenval+1,newly_visited_matrix,x,y,lowest_path))
-    return lowest_path
-
 # Read input file
-#with open("../include/example12.inc","r") as heightmap_file:
 with open("../include/input12.inc","r") as heightmap_file:
     heightmap_matrix = list(map(lambda a: a.strip(),heightmap_file.readlines()))
-
 maxX = len(heightmap_matrix)
 maxY = len(heightmap_matrix[0])
-visited_matrix = ["."*maxY]*maxX
-startX,startY = findChar("E")
-endX,endY = findChar("S")
-print("Part1: %s"%nextSpot(startX,startY,0,visited_matrix,startX,startY,maxX*maxY))
+
+# Search START position
+startX,startY = findChar("S")
+to_visit_list = [(startX,startY)]
+visited_list = []
+path_found = False
+cost_count = 0
+while not path_found:
+    next_list = []
+    # Visit all valid points in this loop
+    for x,y in to_visit_list:
+        # Count loops until END is found
+        if heightmap_matrix[x][y] == "E":
+            cost_count -= 1
+            path_found = True
+            break
+        
+        # Add valid neighbours to list to be visited next loop
+        for i,j in [(-1,0),(1,0),(0,-1),(0,1)]:
+            if (x+i) in range(maxX) and (y+j) in range(maxY) and (x+i,y+j) not in visited_list:
+                if heightVal(heightmap_matrix[x+i][y+j])-heightVal(heightmap_matrix[x][y])<=1:
+                    if (x+i,y+j) not in next_list: next_list.append((x+i,y+j))
+        visited_list.append((x,y))
+    cost_count += 1
+    to_visit_list = next_list.copy()
+
+# Search END position
+endX,endY = findChar("E")
+to_visit_list = [(endX,endY)]
+visited_list = []
+path_found = False
+hike_count = 0
+while not path_found:
+    next_list = []
+    # Visit all valid points in this loop
+    for x,y in to_visit_list:
+        # Count loops until lower point is found
+        if heightVal(heightmap_matrix[x][y]) == ord("a"):
+            hike_count -= 1
+            path_found = True
+            break
+
+        # Add valid neighbours to list to be visited next loop
+        for i,j in [(-1,0),(1,0),(0,-1),(0,1)]:
+            if (x+i) in range(0,maxX) and (y+j) in range(0,maxY) and (x+i,y+j) not in visited_list:
+                if heightVal(heightmap_matrix[x+i][y+j])-heightVal(heightmap_matrix[x][y])>=-1:
+                    if (x+i,y+j) not in next_list: next_list.append((x+i,y+j))
+        visited_list.append((x,y))
+    hike_count += 1
+    to_visit_list = next_list.copy()
+
+print("Part1: %s"%cost_count)
+print("Part2: %s"%hike_count)
